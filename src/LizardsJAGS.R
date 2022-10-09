46b5ccd4eb0358521475a1a5ab7586eebb29a75a
georgewoolsey <george.woolsey@colostate.edu>
2022-10-09 11:55:16 -0600

    updating

---
+++ src/LizardsJAGS.R
@@ -0,0 +1,19 @@
+
+  model{
+    # priors
+    a ~ dnorm(0, 10000)
+    b ~ dnorm(0, 10000)
+    # likelihood
+    for (i in 1:n) {
+      # p[i] <- ilogit(a + b*x[i])
+      logit(p[i]) <- a + b*x[i] 
+      y[i] ~ dbern(p[i])
+    }
+    ## quantities of interest
+      # The predicted probability of occupancy
+      for(j in 1:length(perim_area_z)){
+        p_est[j] <- ilogit(a + b*perim_area_z[j])
+      }
+  
+  }
+  
