882f5ea937b5bca05d4dcaeb0693b92e2166075e
georgewoolsey <george.woolsey@colostate.edu>
2022-10-10 09:06:40 -0600

    updating

--- LogisticJAGS.R
+++ LogisticJAGS.R
@@ -1,28 +1,22 @@
- 
-model {
 
-  # priors
-
-  K ~ dunif(0, 4000)
-  r ~ dunif (0, 2)
-  sigma ~ dunif(0, .5) 
-  tau <- 1/sigma^2
+  ## Logistic example for Primer
+    model{
+      # priors
+      K ~ dunif(0, 4000) # dunif(alpha = lower limit, beta = upper limit)
+      r ~ dunif (0, 2) # dunif(alpha, beta)
+      sigma ~ dunif(0, 2) # dunif(alpha, beta)
+      tau <- 1/sigma^2
+      # likelihood
+      for(i in 1:n){
+        mu[i] <- r - r/K * x[i]
+        y[i] ~ dnorm(mu[i], tau) # dnorm(mu,tau)
+      }
+      ## quantities of interest
+      # population size where the population growth rate is maximum
+      N_max_pop_grwth_rt <- K/2
+      # The rate of population growth
+      for(j in 1:length(N)){
+        pop_grwth_rt[j] <- r * N[j] * (1 - ( N[j] / K ))
+      }
+    }
   
-  # likelihood
-  
-  for(i in 1:n) {
-    mu[i] <- r - r/K * x[i]
-    y[i] ~ dnorm(mu[i], tau)
-  }
-
-  # derived quantities	
-
-  # calculate maximum growth rate	
-  N_at_maxdNdt <- K/2
-  
-  # calculate growth rate over range of N values. N must be read in as data:
-  for (j in 1:length(N)) {
-    dNdt[j] <- r * N[j] * (1 - N[j]/K)
-  }
-} 
-
