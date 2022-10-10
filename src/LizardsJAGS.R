1557783b825f152f6c39514a828679c1978bfb57
georgewoolsey <george.woolsey@colostate.edu>
2022-10-10 06:00:48 -0600

    updating

--- src/LizardsJAGS.R
+++ src/LizardsJAGS.R
@@ -1,12 +1,11 @@
 
   model{
     # priors
-    a ~ dnorm(0, 0.00001)
-    b ~ dnorm(0, 0.00001)
+    a ~ dnorm(0,1E-6)
+    b ~ dnorm(0,1E-6)
     # likelihood
     for (i in 1:n) {
       p[i] <- ilogit(a + b*x[i])
-      # logit(p[i]) <- a + b*x[i] ??? which one
       y[i] ~ dbern(p[i])
     }
     ## quantities of interest
@@ -14,6 +13,9 @@
       for(j in 1:length(perim_area_z)){
         p_est[j] <- ilogit(a + b*perim_area_z[j])
       }
-  
+      # different perimeter-area estimates
+      p_x10 <- exp(a + b*x10) / (1 + exp(a + b*x10))
+      p_x20 <- exp(a + b*x20) / (1 + exp(a + b*x20))
+      diff_x10_x20 <- p_x10 - p_x20
   }
   
